50c835c87a910826cc5397a41a385e78795cf52a
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-03-18 13:29:54 +0300

    link created in create post

---
+++ app/features/blog/create-blog.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../nav/nav';
+import Footer from '../footer/footer';
+
+const CreateBlog = () => {
+  return (
+    <div>
+    <NavBar/>
+   
+    <Footer/>
+    </div>
+  );
+};
+
+export default CreateBlog;
--- app/features/buttons/button.tsx
+++ app/features/buttons/button.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
+
 
 const Button = () => {
   return (
-    <button
-      type="button"
-      className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm  sm:px-6 sm:py-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-    >
-      Create Blog Post
-    </button>
+    <Link href="/create-blog" passHref>
+      <button
+        type="button"
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm sm:px-6 sm:py-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Create Blog Post
+      </button>
+    </Link>
+
   );
 };
 
