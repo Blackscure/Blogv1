2b78a7541fe61381f09a4cb7d5bc4e53772fbdf2
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-03-19 13:16:14 +0300

    single blog created

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,33 @@
-import Footer from '@/app/features/footer/footer'
-import NavBar from '@/app/features/nav/nav'
-import React from 'react'
+import Footer from '@/app/features/footer/footer';
+import NavBar from '@/app/features/nav/nav';
+import React from 'react';
+import blogData from '../../data/blogData';
 
-export default function BlogPost() {
+export default function BlogPost({ params }: { params: { slug: string } }) {
+  // Find the blog post with the matching slug
+  const post = blogData.find(post => post.slug === params.slug);
+
+  // If post is not found, display a message
+  if (!post) {
+    return (
+      <div>
+        <NavBar />
+        <div>Post not found!</div>
+        <Footer />
+      </div>
+    );
+  }
+
+  // Render the details of the matching post
   return (
     <div>
-        <NavBar/>
-   
-        <Footer/>
+      <NavBar />
+      <div>
+        <h1>{post.title}</h1>
+        <p>Date: {post.date}</p>
+        <p>{post.content}</p>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
