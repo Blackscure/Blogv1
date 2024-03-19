b791aaae79591036677fb3dfa67b7cf325d89369
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-03-19 14:09:36 +0300

    jhg

--- app/features/modals/modal.tsx
+++ app/features/modals/modal.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function modal() {
   return (
-      <div className="">
+      <div className="w-full max-w-screen-lg mx-auto mt-8 p-6 bg-white rounded-xl shadow-md ">
             <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
               Create Blog Post
             </h4>
@@ -12,16 +12,39 @@ export default function modal() {
             <form className="mt-2">
             <div className="flex flex-col gap-4">
                 <h6 className="block -mb-3 font-sans text-blue-gray-900">
-                <label htmlFor="name" className="block font-bold">Name</label>
+                <label htmlFor="name" className="block font-medium">Blog Title</label>
                 </h6>
                 <div className="">
                 <input 
                     id="name" 
-                    placeholder=""
-                    className="w-full text-blue-gray-700 outline-0 placeholder-shown:border" 
+                    placeholder="Title"
+                    className="w-full h-8 text-sm text-blue-gray-700 outline-none placeholder-blue-gray-300 border border-blue-gray-200 rounded-md px-3" 
+                />
+                </div>
+
+                                <h6 className="block -mb-3 font-sans text-blue-gray-900">
+                <label htmlFor="name" className="block font-medium">Blog Date</label>
+                </h6>
+                <div className="">
+                <input 
+                    id="name" 
+                    placeholder="Blog date"
+                    className="w-full h-8 text-sm text-blue-gray-700 outline-none placeholder-blue-gray-300 border border-blue-gray-200 rounded-md px-3" 
+                />
+                </div>
+
+                                <h6 className="block -mb-3 font-sans text-blue-gray-900">
+                <label htmlFor="name" className="block font-medium">Slug</label>
+                </h6>
+                <div className="">
+                <input 
+                    id="name" 
+                    placeholder="Slug"
+                    className="w-full h-8 text-sm text-blue-gray-700 outline-none placeholder-blue-gray-300 border border-blue-gray-200 rounded-md px-3" 
                 />
                 </div>
             </div>
+            
             </form>
 
           </div>
